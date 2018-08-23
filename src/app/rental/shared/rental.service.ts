d6335e1c366db280588051634c15bc49899a0c8b
brunocodebox <brunocodebox@gmail.com>
2018-08-23 14:19:38 +0200

    Adds proxy configuration, adds rental details styles improvements, and adds pipes

--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -1,84 +1,21 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "../../../../node_modules/rxjs/Observable";
 import { Rental } from "./rental.model";
+// Added from Section 5, Lecture 63 - Get Rentals from Node Server
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class RentalService {
-  private rentals: Rental[] = [
-    {
-      id: "1",
-      title: "Central Apartment",
-      city: "New York",
-      street: "Times Square",
-      category: "apartment",
-      image: "http://via.placeholder.com/350x250",
-      bedrooms: 3,
-      description: "Very nice apartment",
-      dailyRate: 34,
-      shared: false,
-      createdAt: "16/08/2018"
-    },
-    {
-      id: "2",
-      title: "Central Apartment 2",
-      city: "San Francisco",
-      street: "Main street",
-      category: "condo",
-      image: "http://via.placeholder.com/350x250",
-      bedrooms: 2,
-      description: "Very nice condo",
-      dailyRate: 12,
-      shared: true,
-      createdAt: "16/08/2018"
-    },
-    {
-      id: "3",
-      title: "Central Apartment 3",
-      city: "Bratislava",
-      street: "Hlavna",
-      category: "condo",
-      image: "http://via.placeholder.com/350x250",
-      bedrooms: 2,
-      description: "Very nice condo",
-      dailyRate: 334,
-      shared: true,
-      createdAt: "16/08/2018"
-    },
-    {
-      id: "4",
-      title: "Central Apartment 4",
-      city: "Prague",
-      street: "Argentiska",
-      category: "apartment",
-      image: "http://via.placeholder.com/350x250",
-      bedrooms: 1,
-      description: "Very nice apartment",
-      dailyRate: 50,
-      shared: false,
-      createdAt: "16/08/2018"
-    }
-  ];
+  // Added from Section 5, Lecture 63 - Get Rentals from Node Server
+  constructor(private http: HttpClient) {}
 
   // Explained in Section 3, Lecture 48 - Get Rental Data from Service
-  public getRentalById(rentalId: string): Observable<Rental> {
-    return new Observable<Rental>(observer => {
-      setTimeout(() => {
-        const foundRental = this.rentals.find(rental => {
-          return rental.id == rentalId;
-        });
-
-        // Be aware that the data will be returned in 500 ms so can't be displayed
-        // immediately in the html file. So an ngIf statement will be necessary.
-        observer.next(foundRental);
-      }, 500);
-    });
+  // Explained in Section 5, Lecture 63 - Get Rentals from Node Server
+  public getRentalById(rentalId: string): Observable<any> {
+    return this.http.get("/api/v1/rentals/" + rentalId); // Sends reqwuest to localhost:4200 (default)
   }
 
-  public getRentals(): Observable<Rental[]> {
-    return new Observable<Rental[]>(observer => {
-      setTimeout(() => {
-        observer.next(this.rentals);
-      }, 500);
-    });
+  public getRentals(): Observable<any> {
+    return this.http.get("/api/v1/rentals"); // Sends reqwuest to localhost:4200 (default)
   }
 }
